7999af5fb4f3720ad596599c6cb54870d71b060e
Jongho Lee <jongko@Jonghos-MacBook-Pro.local>
2024-05-26 12:25:32 -0400

    Create xcode project

---
+++ SwiftUIByExample/SwiftUIByExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIByExample
+//
+//  Created by Jongho Lee on 5/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIByExample/SwiftUIByExample/SwiftUIByExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIByExampleApp.swift
+//  SwiftUIByExample
+//
+//  Created by Jongho Lee on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIByExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
