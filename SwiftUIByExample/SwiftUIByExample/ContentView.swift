e08f87148c72b9a6c6b71d40180fde3feb540e8f
Jongho Lee <jongko@Jonghos-MacBook-Pro.local>
2024-05-26 15:50:00 -0400

    Add some Text view

--- SwiftUIByExample/SwiftUIByExample/ContentView.swift
+++ SwiftUIByExample/SwiftUIByExample/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	
+	let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+	
+    var body: some View {	// "some" View -> opaque return types
+//        Text("Hello SwiftUI")
+//			.padding()
+		
+//		List {
+//			Text("1")
+//			Text("2")
+//			Text("3")
+//		}
+		
+		NavigationStack {
+			List {
+				ForEach(menu) { section in
+					
+				}
+				Text("1")
+				Text("2")
+				Text("3")
+			}
+			.navigationTitle("Menu")
+		}
+	}
 }
 
-#Preview {
-    ContentView()
+// Swift 5.9 or higher
+//#Preview {
+//    ContentView()
+//}
+
+struct ContentView_Preview: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
 }
