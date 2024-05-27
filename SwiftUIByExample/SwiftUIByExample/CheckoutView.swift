574b8848329308392c6d95fd3a47fc8397dd357e
Jongho Lee <jongko@Jonghos-MacBook-Pro.local>
2024-05-27 17:32:14 -0400

    Create Form

---
+++ SwiftUIByExample/SwiftUIByExample/CheckoutView.swift
@@ -0,0 +1,62 @@
+//
+//  CheckoutView.swift
+//  SwiftUIByExample
+//
+//  Created by Jongho Lee on 5/27/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+	@EnvironmentObject var order: Order
+	@State private var paymentType = "Cash"
+	@State private var addLoyaltyDetails: Bool = false
+	@State private var loyaltyNumber = ""
+	@State private var tipAmount = 15
+	
+	let paymentTypes = ["Cash", "Credit Card", "Points"]
+	let tipAmounts = [10, 15, 20, 25, 0]
+	
+    var body: some View {
+		Text(paymentType)
+		Form {
+			Section {
+				Picker("How do you want to pay?", 
+					   selection: $paymentType,	// two-way binding(read/write)
+					   content: {
+					ForEach(paymentTypes, id: \.self) {
+						Text($0)
+					}
+				})
+				Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+				if addLoyaltyDetails {
+					TextField("Enter your iDine ID", text: $loyaltyNumber)
+				}
+			}
+			Section("Add a tip?") {
+				Picker("Percentage:",
+					   selection: $tipAmount,
+					   content: {
+					ForEach(tipAmounts, id: \.self) {
+						Text("\($0)%")
+					}
+				})
+				.pickerStyle(.segmented)
+			}
+			Section("Total: $100") {
+				Button("Confirm order") {
+					// place the order
+				}
+			}
+		}
+		.navigationTitle("Payment")
+		.navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+	NavigationStack {
+		CheckoutView()
+			.environmentObject(Order())
+	}
+}
