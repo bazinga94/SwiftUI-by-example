5e490934d04941def9af55576f6ceb7e7e6097c1
Jongho Lee <bazinga940@gmail.com>
2025-09-27 17:53:33 -0500

    Initial iDine

---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Jongho Lee on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iDine/iDineTests/iDineTests.swift
@@ -0,0 +1,17 @@
+//
+//  iDineTests.swift
+//  iDineTests
+//
+//  Created by Jongho Lee on 9/27/25.
+//
+
+import Testing
+@testable import iDine
+
+struct iDineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
